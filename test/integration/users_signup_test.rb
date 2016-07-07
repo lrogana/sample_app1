617f0ca2a7b3d9e86634b158bc767649bb053983
Liston <2ndcenter@gmail.com>
2016-07-07 10:27:15 -0600

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert is_logged_in?
   end
 end
